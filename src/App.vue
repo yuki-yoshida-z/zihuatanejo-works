0bceb9e79052dd3f8c6c52db291f75772b71b8bd
yuki-yoshida-z <yoshida_y@zihuatanejo.jp>
2020-02-06 02:15:23 +0900

    サイドメニューのコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Top</router-link> |
-      <router-link to="/profile">Profile</router-link>
-      <router-link to="/spec">Spec</router-link>
-      <router-link to="/products">Favorite Products</router-link>
-      <router-link to="/contact">Contact</router-link>
+    <global-header/>
+    <div class="main-content">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="stylus">
 #app
   font-family: '游ゴシック体', YuGothic, '游ゴシック', Yu Gothic, 'ヒラギノ角ゴ ProN W3', Hiragino Kaku Gothic ProN, 'メイリオ', Meiryo, sans-serif
+
+.main-content
+  position: relative
+  padding-left: 200px
+  overflow: hidden
+
 </style>
 
 <script>
 import 'ress'
+import GlobalHeader from '@/components/GlobalHeader.vue'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    GlobalHeader
+  }
 }
 </script>
