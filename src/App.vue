ce7aa30a0740275abba79cf02dbcaf28be4a0675
yuki-yoshida-z <yoshida_y@zihuatanejo.jp>
2020-04-08 12:37:44 +0900

    背景svg実装

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,13 @@
   <div id="app">
     <div :class="this.sceneClass">
       <main-header/>
-      <div class="main-content">
+      <div class="main-content" :class="bgStyle">
         <page-curtain/>
         <router-view/>
       </div>
     </div>
   </div>
 </template>
-
-<style lang="sass">
-
-#app
-  font-family: '游ゴシック体', YuGothic, '游ゴシック', Yu Gothic, 'ヒラギノ角ゴ ProN W3', Hiragino Kaku Gothic ProN, 'メイリオ', Meiryo, sans-serif
-
-.main-content
-  position: relative
-  min-height: 100vh
-  padding-left: 200px
-  overflow: hidden
-
-</style>
 
 <script>
 import 'ress'
@@ -43,6 +30,45 @@ export default {
     this.$router.afterEach((to) => {
       this.sceneClass = "scene-" + to.name;
     })
+  },
+  computed: {
+    bgStyle(){
+      return this.$route.name === 'top' ? 'is-hourGlass' : 'is-' + this.$route.name + 'HalfHourGlass'
+    }
   }
 }
 </script>
+
+<style lang="sass">
+
+#app
+  font-family: '游ゴシック体', YuGothic, '游ゴシック', Yu Gothic, 'ヒラギノ角ゴ ProN W3', Hiragino Kaku Gothic ProN, 'メイリオ', Meiryo, sans-serif
+
+.main-content
+  position: relative
+  min-height: 100vh
+  padding-left: 200px
+  overflow: hidden
+  background-color: #fff
+  background-position: right
+  background-size: contain
+
+  .scene-top &
+    background-color: $theme-color-top
+
+  &.is-hourGlass
+    background-image: url("./assets/hourGlass.svg")
+
+  &.is-profileHalfHourGlass
+    background-image: url("./assets/profileBg.svg")
+
+  &.is-specHalfHourGlass
+    background-image: url("./assets/specBg.svg")
+
+  &.is-productsHalfHourGlass
+    background-image: url("./assets/productsBg.svg")
+
+  &.is-contactHalfHourGlass
+    background-image: url("./assets/contactBg.svg")
+
+</style>
