1ae9260da93285c1d3001e114fe348b44abcf1d8
yuki-yoshida-z <yoshida_y@zihuatanejo.jp>
2020-07-01 18:49:54 +0900

    aboutページ修正

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,13 +4,16 @@
     <div class="about-content">
       <div class="about-body">
         <transition name="fade-block" enter-active-class="animated fadeInLeft">
-          <p v-if="blockAnimationStart" class="about-content__text is-first">ZihuatanelightSpeedInjo Worksを閲覧いただきありがとうございます。<br>本サイトは<span class="is-strong is-color">フリーランスウェブエンジニアZihuatanejo</span>の<br>ポートフォリオサイトになります。</p>
+          <p v-if="blockAnimationStart" class="about-body__text is-first">Zihuatanejo Worksを閲覧いただきありがとうございます。<br>本サイトは<span class="is-strong is-color">フリーランスウェブエンジニアZihuatanejo</span>の<br>ポートフォリオサイトになります。</p>
         </transition>
         <transition name="fade-block" enter-active-class="animated fadeInRight">
-          <p v-if="blockAnimationStart" class="about-content__text is-second"><span clalightSpeedInss="is-strong">一緒に仕事をさせていただいている方</span>、<br><span class="is-strong">一緒に仕事をするかどうかご検討中の方</span>、<br><span class="is-strong">ウェブサイトの制作依頼をご検討中の方</span>に<br>少しでも自分のことを知っていただくために制作しました。</p>
+          <p v-if="blockAnimationStart" class="about-body__text is-second"><span class="is-strong">一緒に仕事をさせていただいている方</span>、<br><span class="is-strong">一緒に仕事をするかどうかご検討中の方</span>、<br><span class="is-strong">ウェブサイトの制作依頼をご検討中の方</span>に<br>少しでも自分のことを知っていただくために制作しました。</p>
         </transition>
         <transition name="fade-block" enter-active-class="animated fadeInLeft">
-          <p v-if="blockAnimationStart" class="about-content__text is-third">本サイトがきっかけとなって、<br><span class="is-strong is-color">お互いの発展や新たな出会い</span>に繋がることを心より願っております。<br>どうぞごゆっくりご覧ください。</p>
+          <p v-if="blockAnimationStart" class="about-body__text is-third">本サイトがきっかけとなって、<br><span class="is-strong is-color">お互いの発展や新たな出会い</span>に繋がることを心より願っております。<br>どうぞごゆっくりご覧ください。</p>
+        </transition>
+        <transition name="fade-block" enter-active-class="animated fadeInRight">
+          <p v-if="blockAnimationStart" class="about-body__text is-caution is-forth">なお、本サイトはGitHubに公開してます。ご興味がある方は<a class="about-body__link" href="https://github.com/yuki-yoshida-z/zihuatanejo-works" target="_blank">こちら</a>からご覧ください。</p>
         </transition>
       </div>
     </div>
@@ -50,6 +53,7 @@ export default {
   margin-top: 24px
   padding: 0 7%
 
+.about-body
   &__text
     color: $text-black
     font-size: 2.4vh
@@ -62,10 +66,19 @@ export default {
     &.is-third
       animation-delay: 1s
 
+    &.is-forth
+      animation-delay: 1.5s
+
     .is-color
       color: $theme-color-about
 
     .is-strong
       font-weight: bold
+
+    &.is-caution
+      font-size: 1.8vh
+
+  &__link
+    +text-link($color: $theme-color-about, $hover-color: $theme-color-about)
 
 </style>
