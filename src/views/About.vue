58ace083b7db19b31e51e2f6da1b679d3e1501ef
yuki-yoshida-z <yoshida_y@zihuatanejo.jp>
2020-09-23 11:53:43 +0900

    相互リンク追加

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,7 +14,7 @@
           <p v-if="blockAnimationStart" class="about-body__text is-third nw-thirdText">本サイトがきっかけとなって、<br><span class="is-strong is-color">お互いの発展や新たな出会い</span>に繋がることを心より願っております。<br>どうぞごゆっくりご覧ください。</p>
         </transition>
         <transition name="fade-block" enter-active-class="animate__animated animate__fadeInRight">
-          <p v-if="blockAnimationStart" class="about-body__text is-caution is-forth nw-forthText">なお、本サイトはGitHubに公開してます。ご興味がある方は<a class="about-body__link" href="https://github.com/yuki-yoshida-z/zihuatanejo-works" target="_blank">こちら</a>からご覧ください。</p>
+          <p v-if="blockAnimationStart" class="about-body__text is-caution is-forth nw-forthText">本サイトはGitHubに公開してます。ご興味がある方は<a class="about-body__link" href="https://github.com/yuki-yoshida-z/zihuatanejo-works" target="_blank">こちら</a>からご覧ください。<br>イベントやWebサービスの集客は<a class="about-body__link" href="https://hanata.jp/" target="_blank">こちら</a>をご覧ください。友人のHANATAさんのサイトです。</p>
         </transition>
       </div>
     </div>
